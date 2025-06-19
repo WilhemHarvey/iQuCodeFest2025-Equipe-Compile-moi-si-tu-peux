6ad780cdcdef8289a02ec4053d4be8f583f0012e
LF-Vigneux <156103879+LF-Vigneux@users.noreply.github.com>
2025-06-19 16:07:19 -0400

    Renamed the file

--- Wolfpaq/Night.py
+++ Wolfpaq/Night.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class Night:
     def __init__(self):
-        self.x=3
+        self.x = 3
         pass
 
-    def sorciere(self,person):
+    def sorciere(self, person):
         pass
