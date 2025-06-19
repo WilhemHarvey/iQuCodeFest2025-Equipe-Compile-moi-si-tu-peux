43c09c7d93de730d82bbcb0abfdd7f5f1b20afca
inking02 <ludomartvicotec@gmail.com>
2025-06-19 17:06:23 -0400

    play class

---
+++ play.py
@@ -0,0 +1,18 @@
+from qiskit import QuantumCircuit
+
+class play():
+
+    def __init__(self, nb_players):
+        self.nb_players = nb_players
+
+    
+
+
+    def start_night():
+
+        return NotImplemented
+
+
+
+
+    
