edeef646bca8a613f1f3ac61c365c5bb63b3b837
WilhemHarvey <156103745+WilhemHarvey@users.noreply.github.com>
2025-06-19 17:44:49 -0400

    Clairvoyante

--- Wolfpaq/Night.py
+++ Wolfpaq/Night.py
@@ -3,14 +3,12 @@ from qiskit import QuantumCircuit
 
 
 class Night:
-    def __init__(self, active_players, roles, witch_power, couple = None, first_night = False):
+    def __init__(self, active_players, roles, witch_power, couple = None):
         
         self.qc = QuantumCircuit()
 
-        self.alive = active_players
         self.roles_list = roles
         self.endangered_players=[]
-        self.night_1 = first_night
 
         self.in_love = couple
         self.witch_ability = witch_power
@@ -19,34 +17,50 @@ class Night:
         self.small_attack = np.pi/3
         self.heal = -np.pi/3
 
-    def Werewolf(self, attack_player = None) :
+    def Werewolf(self, attack_player_index) :
+
+        if self.roles_list[attack_player_index] == None :
+            raise ValueError("Trying to kill a dead player")
         
+        self.qc.rx(self.big_attack, 0)
+
+        self.endangered_players.append(attack_player_index)
+
         return
     
     def Witch(self, save_attacked_player = False, attack_player = None, attack_player_index = None):
 
-        if attack_player not in self.alive :
-            raise ValueError("Trying to kill a dead player")
-        
         if save_attacked_player == True and self.witch_ability[0] != False :
             self.qc.rx(self.heal, 0)
             self.witch_ability[0] = False
 
         elif attack_player != None and self.witch_ability[1] != False and attack_player_index !=None :
-
-            if attack_player_index not in self.endegered_players :
+            
+            if self.roles_list[attack_player_index] == None :
+                raise ValueError("Trying to kill a dead player")
+        
+            if attack_player_index not in self.endangered_players :
                 attack_qc = QuantumCircuit(2)
                 attack_qc.rx(self.small_attack/3, 1)
                 self.qc.append(attack_qc, [0,1])
                 self.endangered_players.append(attack_player_index)
 
-            elif attack_player_index in self.endegered_players :
+            elif attack_player_index in self.endangered_players :
                 self.qc.rx(self.small_attack, 0)
 
             else :
                 raise ValueError("error with attack_player_index")
                 
         return
+    
+    def Clairvoyante(self, player_index) :
 
+        if self.roles_list[player_index] == None :
+            raise ValueError("Trying to uncover the role of a dead player")
+    
+        return self.roles_list[player_index]
+    
+    def Savior():
+        return
 
             
