0ea9fe8f2874a2e4a28d8a05c18b778efa92ae60
WilhemHarvey <156103745+WilhemHarvey@users.noreply.github.com>
2025-06-19 17:25:12 -0400

    update nuit

--- Wolfpaq/Night.py
+++ Wolfpaq/Night.py
@@ -5,17 +5,48 @@ from qiskit import QuantumCircuit
 class Night:
     def __init__(self, active_players, roles, witch_power, couple = None, first_night = False):
         
-        self.qc = QuantumCircuit
+        self.qc = QuantumCircuit()
+
         self.alive = active_players
         self.roles_list = roles
-        self.in_love = couple
-        self.night_1 = first_night
         self.endangered_players=[]
+        self.night_1 = first_night
 
-    def Witch(self, save_attacked_player = False):
+        self.in_love = couple
+        self.witch_ability = witch_power
+        
+        self.big_attack = 2*np.pi/3
+        self.small_attack = np.pi/3
+        self.heal = -np.pi/3
 
-        if save_attacked_player == True and self.witch_power[0] != False :
-            self.qc.rx(-np.pi/3, 0)
+    def Werewolf(self, attack_player = None) :
+        
+        return
+    
+    def Witch(self, save_attacked_player = False, attack_player = None, attack_player_index = None):
 
-        elif True:
-            pass
+        if attack_player not in self.alive :
+            raise ValueError("Trying to kill a dead player")
+        
+        if save_attacked_player == True and self.witch_ability[0] != False :
+            self.qc.rx(self.heal, 0)
+            self.witch_ability[0] = False
+
+        elif attack_player != None and self.witch_ability[1] != False and attack_player_index !=None :
+
+            if attack_player_index not in self.endegered_players :
+                attack_qc = QuantumCircuit(2)
+                attack_qc.rx(self.small_attack/3, 1)
+                self.qc.append(attack_qc, [0,1])
+                self.endangered_players.append(attack_player_index)
+
+            elif attack_player_index in self.endegered_players :
+                self.qc.rx(self.small_attack, 0)
+
+            else :
+                raise ValueError("error with attack_player_index")
+                
+        return
+
+
+            
